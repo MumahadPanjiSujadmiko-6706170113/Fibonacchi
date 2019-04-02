085c9de25358733bdd7c5ee6f8a46f18454346f2
Yoga Widhi Kurnia <48684082+yogawidhik@users.noreply.github.com>
2019-04-02 09:37:53 +0700

    Update Main.java

--- RPL8_JURNAL/src/com/company/Main.java
+++ RPL8_JURNAL/src/com/company/Main.java
@@ -8,6 +8,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int n;
+        int jmlh = 0;
 
         System.out.print("Input jumlah deret Fibonacci yang diinginkan : ");
         n = s.nextInt();
@@ -15,8 +16,9 @@ public class Main {
         for (int i = 1; i <= n; i++) {
             int x = cari(i);
             System.out.print(x + " ");
+            jmlh = jmlh + x;
         }
-
+        System.out.println("\nJumlah Output : " + jmlh);
     }
 
     public static int cari(int n) {
